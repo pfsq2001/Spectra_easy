322c59a6d700316be711f5d9a1688cfe6a4b2c46
pfsq2001 <132702952+pfsq2001@users.noreply.github.com>
2024-09-24 10:31:31 -0300

    Update streamlit_roberto.py

--- streamlit_roberto.py
+++ streamlit_roberto.py
@@ -3,7 +3,6 @@ import pandas as pd
 import numpy as np 
 import sys
 import csv
-import matplotlib.pyplot as plt
 import seaborn as sns
 from io import StringIO
 from st_aggrid import AgGrid, GridOptionsBuilder, GridUpdateMode, JsCode
